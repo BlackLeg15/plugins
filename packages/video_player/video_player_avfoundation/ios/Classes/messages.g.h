e8deecfa6525575eaf82590bf186609e5c980d32
Adby Santos <adby.santos15@outlook.com>
2023-01-06 15:39:04 -0300

    fix: player on iOS (2)

--- packages/video_player/video_player_avfoundation/ios/Classes/messages.g.h
+++ packages/video_player/video_player_avfoundation/ios/Classes/messages.g.h
@@ -126,6 +126,7 @@ NSObject<FlutterMessageCodec> *FLTAVFoundationVideoPlayerApiGetCodec(void);
 - (void)pause:(FLTTextureMessage *)msg error:(FlutterError *_Nullable *_Nonnull)error;
 - (void)setMixWithOthers:(FLTMixWithOthersMessage *)msg
                    error:(FlutterError *_Nullable *_Nonnull)error;
+- (void)setupMux:(FLTMuxConfigMessage*)input error:(FlutterError *_Nullable *_Nonnull)error;
 @end
 
 extern void FLTAVFoundationVideoPlayerApiSetup(
